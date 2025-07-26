b9ad885739d5ae31a3b6854cebe8879cbb6f7757
Adam Marcus <adam@mac.myfiosgateway.com>
2025-07-26 16:37:54 -0400

    removed docker vpce

--- lib/dw-stack.ts
+++ lib/dw-stack.ts
@@ -85,17 +85,13 @@ export class DataWarehouseStack extends Stack {
       service: ec2.InterfaceVpcEndpointAwsService.SECRETS_MANAGER,
     });
     
-    // vpc.addInterfaceEndpoint('CodeBuildLogsEndpoint', {
-    //   service: ec2.InterfaceVpcEndpointAwsService.CLOUDWATCH_LOGS,
+    // vpc.addInterfaceEndpoint('EcrApiEndpoint', {
+    //   service: ec2.InterfaceVpcEndpointAwsService.ECR,
     // });
-
-    vpc.addInterfaceEndpoint('EcrApiEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR,
-    });
     
-    vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
-      service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
-    });
+    // vpc.addInterfaceEndpoint('EcrDockerEndpoint', {
+    //   service: ec2.InterfaceVpcEndpointAwsService.ECR_DOCKER,
+    // });
     
     const rdsSecurityGroup = new ec2.SecurityGroup(this, 'RDSSecurityGroup', {
       vpc,
