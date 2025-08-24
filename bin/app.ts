7122adc2c9490163658706f9f5d266371258ee60
Adam Marcus <adam@mac.myfiosgateway.com>
2025-08-23 21:58:29 -0400

    fixed ec2 bug

--- bin/app.ts
+++ bin/app.ts
@@ -4,7 +4,12 @@ import { PipelineStack } from '../lib/cdk-pipeline-stacks';
 
 const app = new App();
 
-new  DataWarehouseStack(app, 'DataWarehouseStack');
+new  DataWarehouseStack(app, 'DataWarehouseStack',  {
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION,
+    },
+  },);
 
 new  PipelineStack(app, 'PipelineStack');
 
